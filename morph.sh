b2723a153601a4affc2d6dd1f56a6bda7ab2cc30
YeZ <88169249+1Xy0036@users.noreply.github.com>
2024-06-18 00:30:07 +0800

    Update morph.sh

--- morph.sh
+++ morph.sh
@@ -22,21 +22,21 @@ handle_choice() {
             echo "安装节点..."
             mkdir -p ~/.morph 
             cd ~/.morph
-            git clone https://github.com/morph-l2/morph.git
+            if [ ! -d "morph" ]; then
+                git clone https://github.com/morph-l2/morph.git
+            fi
             cd morph
             git checkout v0.1.0-beta
             make nccc_geth
             cd ~/.morph
-            wget https://raw.githubusercontent.com/morph-l2/config-template/main/holesky/data.zip
-            unzip data.zip
-            cd ~/.morph
+            wget https://raw.githubusercontent.com/morph-l2/config-template/main/holesky/data.zip -O data.zip
+            unzip -o data.zip
             openssl rand -hex 32 > jwt-secret.txt
             echo "===========安装完成============"
             ;;
         2)
             echo "启动节点..."
-            screen -S morphNode
-            ./morph/go-ethereum/build/bin/geth --morph-holesky \
+            screen -dmS morphNode bash -c './morph/go-ethereum/build/bin/geth --morph-holesky \
             --datadir "./geth-data" \
             --http --http.api=web3,debug,eth,txpool,net,engine \
             --authrpc.addr localhost \
@@ -44,24 +44,22 @@ handle_choice() {
             --authrpc.port 8551 \
             --authrpc.jwtsecret=./jwt-secret.txt \
             --miner.gasprice="100000000" \
-            --log.filename=./geth.log
-            # 按下 Ctrl+A，然后按 D 将会话分离
-
+            --log.filename=./geth.log'
+            echo "节点已在后台启动。使用 screen -r morphNode 查看节点运行情况。"
             ;;
         3)
             echo "查看gethLog"
-            screen -S gethLog
-            tail -f geth.log
+            screen -dmS gethLog bash -c 'tail -f ~/.morph/geth.log'
+            echo "日志已在后台查看。使用 screen -r gethLog 查看日志。"
             ;;
         4)
             echo "查看节点是否正常"
-            screen -S netInfo
             curl http://localhost:26657/net_info
             ;;
         5)
             echo "查看同步状态..."
             curl http://localhost:26657/status
-            echo "catching_up”表示节点是否同步。True 表示它是同步的。同时，返回的latest_block_height表示该节点同步的最新块高度。"
+            echo "catching_up表示节点是否同步。True 表示它是同步的。同时，返回的latest_block_height表示该节点同步的最新块高度。"
             ;;
         *)
             echo "无效的选项，请输入1-5之间的数字。"
